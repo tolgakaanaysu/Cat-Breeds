7b2be53968fb75ea48b58b277f1cceb01de2374d
Tolga Kağan <tolga.kagan@hotmail.com>
2022-04-16 10:11:34 +0300

    Initial Commit

---
+++ Cat Breeds/Cat_BreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_BreedsApp.swift
+//  Cat Breeds
+//
+//  Created by Tolga Kağan Aysu on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Cat_BreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat Breeds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cat Breeds
+//
+//  Created by Tolga Kağan Aysu on 16.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
